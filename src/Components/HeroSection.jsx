7a161b2b0e171d00fe0cd4ba9b346d0f994a634e
Arun jR <arun@Aruns-MacBook-Pro.local>
2024-04-28 01:13:43 +0545

    h1 was not in center and fixed it

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -7,7 +7,7 @@ const HeroSection = () => {
           className='bg-cover w-full bg-center relative -z-10'
       >
           <div className='mb-4 absolute top-1/2 left-1/4 w-1/2 text-white text-center'>
-              <h2 className='text-white flex  mr-10 mb-4 mt-0 text-4xl ' style={{fontWeight:'1000'}}>Innovate, Collaborate, Thrive</h2>
+              <h2 className='text-white  mr-10 mb-4 mt-0 text-4xl ' style={{fontWeight:'1000'}}>Innovate, Collaborate, Thrive</h2>
               <p className='text-white text-x1 px-24 py-2 ' style={{
                 fontWeight:'300', fontFamily:'Noto, sans-serif' 
                 
